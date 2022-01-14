7c2c888981be9bbcd2719fef179c6c56492a8e9c
jum0624 <jum0624@naver.com>
2022-01-14 17:11:57 +0900

    add: 회원 서비스 테스트

--- 실습코드/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 실습코드/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
      * 회원 가입
@@ -40,8 +43,8 @@ public class MemberService {
     }
 
     /*
-    * 전체 회원 조회
-    */
+     * 전체 회원 조회
+     */
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
